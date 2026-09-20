5a1b91ed8d9a757a1bc38fdf1fd9984c6490f8a4
Lucas Rouzaire <lucasrouzaire12@gmail.com>
2026-09-20 19:09:03 +0200

    Partie Modèle partiellement faite (pas compris ...)

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35ee45674f71e658de209cee79398edf58b434f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+642b56119abbacd6a5d9921e28f28d17a75c1c60")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
